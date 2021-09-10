b54d0c875c2600f76d304ea3ac53f7ec418aa683
Changmin Choi <cmchoi9901@gmail.com>
2021-09-11 02:07:29 +0900

    finish homework-2

--- homework-2/main.c
+++ homework-2/main.c
@@ -3,20 +3,30 @@
 //
 /// @file
 /// @brief main file
-/// @author <yourname>
-/// @studid <studentid>
+/// @author Changmin Choi
+/// @studid 2017-19841
 #include <stdio.h>
 #include <stdlib.h>
+#include "mathlib.h"
+
 
 /// @brief program entry point
 int main(int argc, char *argv[])
 {
   // first print all command line arguments
-  // TODO
+  printf("Command line arguments (%d total):\n", argc);
+  for(int i = 0; i < argc; i++){
+    printf("   %d: '%s'\n", i, argv[i]);
+  }
 
+  printf("\n");
   // if there is at least one command line argument,
   // add them all up and print the sum.
-  // TODO
+  int s = 0;
+  for(int i = 1; i < argc; i++){
+    s = add(s, atoi(argv[i]));
+  }
+  printf("Sum of arguments: %d\n", s);
 
   return EXIT_SUCCESS;
 }
---
+++ homework-2/mathlib.h
@@ -0,0 +1,13 @@
+//--------------------------------------------------------------------------------------------------
+// System Programming                     Homework #2                                    Fall 2021
+//
+/// @file
+/// @brief math library header file
+/// @author Changmin Choi
+
+/// @brief returns the sum of two integer values
+/// @param a term a
+/// @param b term b
+/// @retval sum of a + b
+int add(int a, int b);
+
